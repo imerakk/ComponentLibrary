15dbd16819d1cff65d6b7a0ccec7d4a3bc7c1916
liuchunxi <liuchunxi@hebao.im>
2018-10-16 18:46:06 +0800

    Initial Commit

---
+++ ComponentLibrary/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ComponentLibrary
+//
+//  Created by liuchunxi on 2018/10/16.
+//  Copyright © 2018年 imera. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ ComponentLibrary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ComponentLibrary
+//
+//  Created by liuchunxi on 2018/10/16.
+//  Copyright © 2018年 imera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
