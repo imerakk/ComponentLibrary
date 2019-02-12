bf329ab4727c10b57096c34d948b11e8540dc2a0
imera <386418132@qq.com>
2019-02-12 18:31:58 +0800

    【修改】调整项目目录

---
+++ ComponentLibrary/Utility/GTSnapshotCapture.h
@@ -0,0 +1,20 @@
+//
+//  GTSnapshotCapture.h
+//  ComponentLibrary
+//
+//  Created by liuchunxi on 2019/2/12.
+//  Copyright © 2019年 imera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GTSnapshotCapture : NSObject
+
++ (UIImage *)imageWithScreenSnapshot;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ComponentLibrary/Utility/GTSnapshotCapture.m
@@ -0,0 +1,19 @@
+//
+//  GTSnapshotCapture.m
+//  ComponentLibrary
+//
+//  Created by liuchunxi on 2019/2/12.
+//  Copyright © 2019年 imera. All rights reserved.
+//
+
+#import "GTSnapshotCapture.h"
+
+@implementation GTSnapshotCapture
+
++ (UIImage *)imageWithScreenSnapshot {
+//    UIGraphicsBeginImageContext(<#CGSize size#>)
+    
+    return nil;
+}
+
+@end
